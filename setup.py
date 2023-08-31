badfa65565e6fc92bfe9eb88be9335304a108c01
Paul KH KIM <babo_81@hotmail.com>
2023-08-31 12:07:40 +1200

    setup.py - version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='tnzapi',
-    version='2.3.0.0',
+    version='2.3.0.1',
     description='TNZ REST API Helper Library for Python',
     long_description=readme(),
     long_description_content_type='text/markdown',
